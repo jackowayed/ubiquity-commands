420acee0e28a2af5c937eff398f11372ad824fee
Daniel Jackoway <daniel@daniel-laptop.(none)>
2009-01-16 16:24:50 -0500

    first commit, rubyforge working

---
+++ README.markdown
@@ -0,0 +1,10 @@
+# Ubiquity Commands
+## Random (hopefully) useful things I've written
+
+These are all licensed under the MIT license. Basically, you're free to use them, copy them, change them, etc. so long as you keep the copyright and attribution info with the code that you use. See the COPYING file for the full text. In COPYING I also make the inclusion of the copyright notice less strict if you are using my code as a sample to make your own commands. 
+
+These are all commands adding functionality to the [Ubiquity Firefox Extension](http://ubiquity.mozilla.com). 
+
+All I ask is that if you like them, tell people about it so that the community gets more use out of my work. 
+
+And if you want to make them better, talk to me ([jackowayed](http://twitter.com/jackowayed) on twitter and jackowayed "at" gmail) or just fork my [github repo](http://github.com/jackowayed/ubiquity-commands/tree/master) and make the changes. 
---
+++ rubyforge.js
@@ -0,0 +1,26 @@
+//rubyforge search command copyright Daniel Jackoway begin
+    makeSearchCommand({
+    name: "rubyforge",
+    author: {name: "Daniel Jackoway", email: "jackowayed@gmail.com", twitter: "jackowayed"},
+    homepage: "http://jackowayed.wordpress.com/",
+    takes: {"Query": noun_arb_text},
+    license: "MIT",
+    url: "http://rubyforge.org/search/?type_of_search=soft&Search=Search&words={QUERY}",
+    description: "searches rubyforge.org, a rubygem hosting site",
+    help: "Type 'rubyforge foo bar' to search rubyforge projects for 'foo bar'",
+    preview: function(pblock, directObject){
+    if (directObject.text<1){pblock.innerHTML = "Search <b>RubyForge</b>"; return;}
+      jQuery.get(this.url.replace("{QUERY}", directObject.text), function(doc){
+      var tempElement = CmdUtils.getHiddenWindow().document.createElementNS("http://www.w3.org/1999/xhtml", "div");
+tempElement.innerHTML = doc.replace(/href=\"\//g, "href=\"http:\/\/rubyforge.org\/");
+  var results = jQuery("table[cellspacing='2']:first", tempElement);
+jQuery("tr", results).css("background-color", "#000");
+pblock.innerHTML="<table style='background-color: #000'>"+results.html()+"</table>";
+
+     })
+        }
+    //execute: function(directObject) {
+    //Utils.openUrlInBrowser(this.url.replace("{QUERY}",directObject.text));
+
+    //}
+  })//rubyforge search command end
